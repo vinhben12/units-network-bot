a938211d7447acf18e3f6dec3431e736b7f9d2e5
beacon <75365338+vinhben12@users.noreply.github.com>
2024-08-03 20:14:50 +0700

    displayHeader.js

--- src/displayHeader.js
+++ src/displayHeader.js
@@ -4,8 +4,6 @@ function displayHeader() {
   process.stdout.write('\x1Bc');
   console.log(colors.cyan('========================================'));
   console.log(colors.cyan('=      Units Network Testnet BOT       ='));
-  console.log(colors.cyan('=     Created by HappyCuanAirdrop      ='));
-  console.log(colors.cyan('=    https://t.me/HappyCuanAirdrop     ='));
   console.log(colors.cyan('========================================'));
   console.log();
 }
